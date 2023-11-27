2e7a40dfa5b98c8fa5b609faa5837fb7610f5e7b
Dom <dom.benatar@hotmail.com>
2023-11-27 18:07:12 +0000

    blog

---
+++ blogdog/src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="text-lime-500">Made by Dom 2023</footer>
+    </>
+  );
+}
---
+++ blogdog/src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Blog Dog's dog blog</h1>
+    </div>
+  );
+}
